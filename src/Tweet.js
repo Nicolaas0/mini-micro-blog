5d1a70d635a5e156a1d40ea676461575b588e0b9
Nicolaas0 <nicolerrick@gmail.com>
2021-04-17 01:12:49 +0700

    create tweet component

---
+++ src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+function Tweet() {
+    return (
+        <div className='tweet'>
+            <h3>Name</h3>
+            <p>Hello twitt!!</p>
+            <h3>Number Of likes</h3>
+      </div>  
+    );
+}
+
+export default Tweet;
