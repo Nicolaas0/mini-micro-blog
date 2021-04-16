f57ef3e0b4498838e14c128784f09e93a1750c84
Nicolaas0 <nicolerrick@gmail.com>
2021-04-16 20:37:51 +0700

    first function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function app() {
+
+  function hello() {
+    console.log('Hello!');
+  }
+
   return (
     <div>
       <h1>Hello react!!</h1>
+      <button onClick={hello}>Click me!</button>
     </div>
   );
 }
