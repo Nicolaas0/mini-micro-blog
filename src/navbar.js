31cc938b19ef6c1733cb9a5eb18d17a37e603f58
nicolaas <nicolerrick@gmail.com>
2021-04-18 22:08:58 +0700

    done creating basic navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './App.css';
 
 const Navbar = () => {
     return (
-          <div>Hello</div>
+        <div>
+            <div className='logo'><h2>Blog</h2></div>
+            <div id='search'><input type="text" placeholder='Search' /></div>
+            <div id='selection'>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
     );
 }
  
