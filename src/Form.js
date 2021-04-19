0dff7483d189f28674b227f0f12ce6a8169ec6b8
Nicolaas0 <nicolerrick@gmail.com>
2021-04-19 23:57:27 +0700

    creating form components

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Form = () => {
+  return (
+    <div>
+      <div className="container">
+        <form action=""></form>
+      </div>
+    </div>
+  );
+};
