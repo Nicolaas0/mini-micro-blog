bc7f3adea3c01d229512a1af444ed2a061053c1e
Nicolaas0 <nicolerrick@gmail.com>
2021-04-20 21:14:45 +0700

    redoing the form component

--- src/Form.js
+++ src/Form.js
@@ -5,13 +5,7 @@ const Form = () => {
 
   return (
     <div className='form'>
-      <div className="container">
-        <form action="">
-          <label htmlFor="">What Do you think?</label>
-          <input type="text"/>
-          <button>Submit</button>
-        </form>
-      </div>
+
     </div>
   );
 };
