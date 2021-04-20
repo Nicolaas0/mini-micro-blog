e23deab9291ec6c199b2b3c91fd2fd85acfa08bc
Nicolaas0 <nicolerrick@gmail.com>
2021-04-20 19:49:53 +0700

    add and import form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './navbar';
+import Form from './Form'
 
 function app() {
   return (
     <div className='App'>
       <Navbar />
       <div className='content'>
-        
+        <Form />
       </div>
     </div>
   );
