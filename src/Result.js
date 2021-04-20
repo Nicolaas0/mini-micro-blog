66db586d94cc8913ee2822626df28b4718feffd5
Nicolaas0 <nicolerrick@gmail.com>
2021-04-20 21:29:04 +0700

    creating result example

--- src/Result.js
+++ src/Result.js
@@ -3,11 +3,38 @@ import './index.css';
 import { useState } from 'react';
 
 const Result = () => {
-    const [res, setRes] = useState('Your thoughts will be here...')
+    const [data, setData] = useState( [
+      {
+        username: "@nicolaas",
+        message: "Wow it is so cool",
+        like: 120,
+        id: 1,
+      },
+      {
+        username: "@nadine",
+        message: "no it is so dumb!",
+        like: 2,
+        id: 2,
+      },
+      {
+        username: "@49384029",
+        message: "Get a promo in hihi Chicken, 30% Off!",
+        like: 0,
+        id: 3,
+      }
+    ]);
 
     return (
         <div className='result'>
-            <p>{res}</p>
+            {
+                data.map((d) => (
+                    <div className='dataPrev' key={d.id}>
+                        <h3>{d.username}</h3>
+                        <p>{d.message}</p>
+                        <div>Like:{d.like }</div>
+                    </div>
+                ))
+            }
         </div>
     );
 }
