25ec9a80df3b6b29d134b96649e4bff458b6e7a9
Nicolaas0 <nicolerrick@gmail.com>
2021-04-22 20:36:27 +0700

    prettier

--- src/resultShow.js
+++ src/resultShow.js
@@ -8,10 +8,14 @@ const ResultShow = (prop) => {
     <div className="result-show">
       {data.map((d) => (
         <div className="dataPrev" key={d.id}>
-          <div id='uname'>{d.username}</div>
-          <p id='msg'>{d.message}</p>
-          <div id='like'>Like:{d.like}</div>
-          <button onClick={() => handleDelete(d.id)}>Delete</button>
+          <div id="uname">{d.username}</div>
+          <p id="msg">{d.message}</p>
+          <div id="like">Like:{d.like}</div>
+          <img
+            id="ic"
+            onClick={() => handleDelete(d.id)}
+            src="https://img.icons8.com/material-outlined/50/000000/add-trash.png"
+          />
         </div>
       ))}
     </div>
