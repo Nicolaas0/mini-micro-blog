17121d4bf3b02443cd83967271f0d3d2510d2079
Nicolaas0 <nicolerrick@gmail.com>
2021-04-23 23:56:02 +0700

    delete the array and useEffect for fetching data

--- src/Result.js
+++ src/Result.js
@@ -1,40 +1,34 @@
 import React from 'react';
 import './index.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ResultShow from './resultShow';
 
 const Result = () => {
-    const [data, setData] = useState( [
-      {
-        username: "@nicolaas",
-        message: "Wow it is so cool",
-        like: 120,
-        id: 1,
-      },
-      {
-        username: "@nadine",
-        message: "no it is so dumb!",
-        like: 2,
-        id: 2,
-      },
-      {
-        username: "@49384029",
-        message: "Get a promo in hihi Chicken, 30% Off!",
-        like: 0,
-        id: 3,
-      }
-    ]);
+    const [data, setData] = useState(null);
   
   const handleDelete = (id) => {
     const del = data.filter(dat => dat.id !== id);
     setData(del);
   }
 
+  useEffect(
+    () => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setData(data);
+        });
+    },
+    [],
+  );
+
   return (
     <React.StrictMode>
       <div>
-        <ResultShow val={data} del={handleDelete} />
-        <ResultShow val={data.filter((dat)=>dat.username === '@nicolaas')} title='Nicolaas comments'/>
+        {data&& <ResultShow val={data} del={handleDelete} />}
+        {/* <ResultShow val={data.filter((dat)=>dat.username === '@nicolaas')} title='Nicolaas comments'/> */}
       </div>
     </React.StrictMode>
   );
