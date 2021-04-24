3465ccdf398953819d5db73a2e968461de04dc3c
Nicolaas0 <nicolerrick@gmail.com>
2021-04-24 19:56:48 +0700

    deleting the logic

--- src/Result.js
+++ src/Result.js
@@ -1,46 +1,17 @@
 import React from "react";
 import "./index.css";
-import { useState, useEffect } from "react";
 import ResultShow from "./resultShow";
+import useFetch from "./useFetch";
 
 const Result = () => {
-  const [data, setData] = useState(null);
-  const [isLoading, setisLoading] = useState(true);
-  const [isError, setError] = useState(null);
-
-  const handleDelete = (id) => {
-    const del = data.filter((dat) => dat.id !== id);
-    setData(del);
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Error fetching data.");
-          }
-          console.log(res);
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setisLoading(false);
-          setError(null)
-        })
-        .catch((err) => {
-          setisLoading(false);
-          setError(err.message)
-        });
-    }, 1000);
-  }, []);
+  const {data, isLoading, isError } = useFetch("http://localhost:8000/blogs");
 
   return (
     <React.StrictMode>
       <div>
         {isError && <div>{isError}</div>}
         {isLoading && <div>Loading...</div>}
-        {data && <ResultShow val={data} del={handleDelete} />}
+        {data && <ResultShow val={data} />}
       </div>
     </React.StrictMode>
   );
