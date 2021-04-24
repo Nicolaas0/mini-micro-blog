c8ba916fbafadaa81274977c43dea651dd84be2f
Nicolaas0 <nicolerrick@gmail.com>
2021-04-24 19:33:54 +0700

    showing the error mesasge if its error

--- src/Result.js
+++ src/Result.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import './index.css';
-import { useState, useEffect } from 'react';
-import ResultShow from './resultShow';
+import React from "react";
+import "./index.css";
+import { useState, useEffect } from "react";
+import ResultShow from "./resultShow";
 
 const Result = () => {
-    const [data, setData] = useState(null);
-    const [isLoading, setisLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const [isError, setError] = useState(null);
 
   const handleDelete = (id) => {
-    const del = data.filter(dat => dat.id !== id);
+    const del = data.filter((dat) => dat.id !== id);
     setData(del);
-  }
+  };
 
-  useEffect(
-    () => {
-      setTimeout(()=>{
-              fetch('http://localhost:8000/blogs')
-                .then(res => {
-                  console.log(res);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Error fetching data.");
+          }
+          console.log(res);
           return res.json();
         })
-        .then(data => {
+        .then((data) => {
           setData(data);
           setisLoading(false);
+          setError(null)
         })
-                .catch(err => {
-                  console.log(err.message);
-        })
-      },1000)
-    },
-    []
-  );
+        .catch((err) => {
+          setisLoading(false);
+          setError(err.message)
+        });
+    }, 1000);
+  }, []);
 
   return (
     <React.StrictMode>
       <div>
+        {isError && <div>{isError}</div>}
         {isLoading && <div>Loading...</div>}
-        {data&& <ResultShow val={data} del={handleDelete} />}
-        {/* <ResultShow val={data.filter((dat)=>dat.username === '@nicolaas')} title='Nicolaas comments'/> */}
+        {data && <ResultShow val={data} del={handleDelete} />}
       </div>
     </React.StrictMode>
   );
-}
+};
 
 export default Result;
