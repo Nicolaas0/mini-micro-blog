421ea926a7f5a45866e4f570f2643a2231541a81
Nicolaas0 <nicolerrick@gmail.com>
2021-04-25 22:22:57 +0700

    exacting path to home and create path to Create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Navbar from './navbar';
-import Result from './Result'
+import Result from './Result';
+import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function app() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Switch>
-          <Route path='/'>
-            <Result/>
-          </Route>
-          <Result />
-        </Switch>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Result />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      </div>
-      </Router>
+    </Router>
   );
 }
 
