8ddda69701073194c259cb6c157cc2b40350433b
Nicolaas0 <nicolerrick@gmail.com>
2021-04-26 23:26:24 +0700

    move the BDShow to the article tag

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React from "react";
+import "./index.css";
+import useFetch from "./useFetch";
+import BDShow from './BDShow';
+import { useParams } from "react-router";
 
-const blogDetails = (prop) => {
-    const data = prop.val;
-   
-    return (
-      <div className="blog-details">
-        {data.map((d) => (
-          <div className="dataPrev">
-            <div id="uname">{d.username}</div>
-            <p id="msg">{d.message}</p>
-            <div id="like">Like:{d.like}</div>
-            <img
-              className="ic"
-              src="https://img.icons8.com/material-sharp/50/000000/facebook-like.png"
-            />
-            <img
-              className="ic"
-              src="https://img.icons8.com/material-sharp/24/000000/thumbs-down.png"
-            />
-            <img
-              className="ic"
-              src="https://img.icons8.com/material-outlined/50/000000/add-trash.png"
-            />
-          </div>
-        ))}
-      </div>
-    );
-}
+const BlogDetails = () => {
+  const { id } = useParams();
+  const { data, isLoading, isError } = useFetch("http://localhost:8000/blogs/" + id);
 
-export default blogDetails;
+  return (
+    <div className="dataPrev">
+      {isError && <div>{isError}</div>}
+      {isLoading && <div>Loading...</div>}
+      {data && (
+        <article>
+          <h2>{data.username}</h2>
+          <p id="msg">{data.message}</p>
+          <div id="like">Like:{data.like}</div>
+          <img
+            className="ic"
+            src="https://img.icons8.com/material-sharp/50/000000/facebook-like.png"
+          />
+          <img
+            className="ic"
+            src="https://img.icons8.com/material-sharp/24/000000/thumbs-down.png"
+          />
+          <img
+            className="ic"
+            src="https://img.icons8.com/material-outlined/50/000000/add-trash.png"
+          />
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
