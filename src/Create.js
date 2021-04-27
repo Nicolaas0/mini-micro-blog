55f29fa2b208cebf6853fa2288181c1e179ce7ed
Nicolaas0 <nicolerrick@gmail.com>
2021-04-27 22:37:06 +0700

    create a post request to post the blog

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Create = () => {
-    return (<div>
-        <div className="create">
-            <label htmlFor="">Username</label>
-            <input type="text" name='uname' />
-            <label htmlFor="">Message</label>
-            <input type="text" name='mes'/>
-        </div>
-    </div> );
-}
- 
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const [like, setLike] = useState("");
+  const [isPending, setIsPending] = useState(false);
+  let l = Math.floor(Math.random() * 10);
+
+  const likeGen = () => {
+    setLike(l);
+  };
+
+  const fixUname = (u) => {
+    return '@'+u;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let v = { username, message, like };
+    const un = fixUname(v.username);
+    v.username = un;
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(v),
+    }).then(() => {
+      setIsPending(false);
+    });
+  };
+
+  useEffect(() => {
+    //add dependencies for l so it didnt create infinite loop
+  }, [l]);
+
+  return (
+    <div>
+      <div className="create">
+        <form onSubmit={handleSubmit}
+        >
+          <label>Username:</label>
+          <input
+            type="text"
+            required
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <label>Message</label>
+          <textarea
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          {!isPending && <button onClick={likeGen}>Submit</button>}
+          {isPending && <button onClick={likeGen}>Adding...</button>}
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default Create;
